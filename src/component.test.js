c0458012f2763d94074868a19af6d7eadc2202b0
Tim Ambler <tim.ambler@getbuilt.com>
2019-07-05 09:34:19 -0500

    remove debugger calls

--- src/component.test.js
+++ src/component.test.js
@@ -1,24 +1,8 @@
-console.log('herp');
-
-import MyComponent from './component.js';
-// import { render } from 'react-testing-library';
-
-debugger;
-
-console.log('foo');
-
 describe('MyComponent', () => {
-
-  debugger;
 
   it('Should work', () => {
 
-    debugger;
-
     expect(true).toEqual(true);
-
-    // const component = render(<MyComponent />);
-    // console.log(component);
 
   });
 
